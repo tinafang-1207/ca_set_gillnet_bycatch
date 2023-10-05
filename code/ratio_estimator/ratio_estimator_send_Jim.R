e27649122aad66d1cc0b5120e03b0910ef52382c
tinafang-1207 <yutianfang@ucsb.edu>
2023-10-05 15:04:27 -0700

    add some changes to ratio estimator script

---
+++ code/ratio_estimator/ratio_estimator_send_Jim.R
@@ -0,0 +1,11 @@
+
+##### clean working environment #####
+rm(list = ls())
+
+#### load in package ####
+library(tidyverse)
+
+#### read in logbook data ####
+lb_orig <- readRDS()
+
+obs_orig <- readRDS()
