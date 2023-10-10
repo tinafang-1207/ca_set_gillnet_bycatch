eee416ee78a9d0f47f1d8d52c128a1b13b715b12
tinafang-1207 <yutianfang@ucsb.edu>
2023-10-10 13:32:03 -0700

    make changes to create figure

--- code/ratio_estimator/ratio_estimator_send_Jim.R
+++ code/ratio_estimator/ratio_estimator_send_Jim.R
@@ -1,11 +1,16 @@
 
-##### clean working environment #####
+### clean working environment ###
 rm(list = ls())
 
-#### load in package ####
+### load in package ###
 library(tidyverse)
 
-#### read in logbook data ####
-lb_orig <- readRDS()
+### read in logbook and observer data ###
+lb_orig <- readRDS("data/confidential/processed/processed_CDFW_logbook/CDFW_1980_2022_gillnet_logbook_new_final.Rds")
 
-obs_orig <- readRDS()
+obs_orig <- readRDS("data/confidential/processed/processed_obs_1980_2017/obs_1980_2017_final.Rds")
+
+### Explore fishing efforts stratified by quarter and geographic regions ###
+
+
+
