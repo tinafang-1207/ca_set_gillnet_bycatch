164f2f5d46508b5ac88159bd6c2af64dbd208d00
tinafang-1207 <yutianfang@ucsb.edu>
2023-12-01 17:09:14 -0800

    update model prediction script

---
+++ code/modeling/Step_4_modeling_rf_prediction.R
@@ -0,0 +1,14 @@
+
+# clean working space
+rm(list = ls())
+
+# read in packages 
+library(tidyverse)
+
+# read in predict data
+
+# notice: this is the fake data to show the spatial pattern
+
+data_predict <- readRDS("data/confidential/processed/pre_model/sealion/fake_spatial_pre_model.Rds")
+
+
