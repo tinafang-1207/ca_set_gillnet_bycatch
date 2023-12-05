b2392f9b705f243a69ca68d89158e9ffa688f3d7
tinafang-1207 <yutianfang@ucsb.edu>
2023-12-04 17:03:01 -0800

    work on model prediction of spatial risk

--- code/modeling/Step_4_modeling_rf_prediction.R
+++ code/modeling/Step_4_modeling_rf_prediction.R
@@ -5,9 +5,17 @@ rm(list = ls())
 # read in packages 
 library(tidyverse)
 
+library(tidymodels)
+library(ranger)
+library(workflows)
+library(randomForest)
+
+
 # read in predict data
 
 # notice: this is the fake data to show the spatial pattern
+
+# The SST needs to fix in the future (we want to have average SST from 2010 rather than 2000)
 
 data_predict <- readRDS("data/confidential/processed/pre_model/sealion/fake_spatial_pre_model.Rds")
 
@@ -36,8 +44,11 @@ hs <- output_hs_balanced[["final_fit"]][["model_smote_final_fit"]]
 # soupfin shark - SMOTE
 ss <- output_ss_balanced[["final_fit"]][["model_smote_final_fit"]]
 
+###########################################################
+################### model prediction ######################
+###########################################################
 
-######## model prediction ########
+
 
 # Format the predict data
 
@@ -84,9 +95,61 @@ predict_best_model <- function(best_model_fit, predict_data){
 }
 
 
-# this does not work now
+# this does not work now (need to change the column names!)
 
-# so I will just predict one by one
+# so I will just predict one by one (this will change later!)
+
+# sealion
+
+preds_fac_sl <- predict(sl, predict_data_format)
+preds_prob_sl <- predict(sl, predict_data_format, type = "prob")
+preds_sl_final <-  predict_data_format %>%
+  bind_cols(preds_fac_sl) %>%
+  bind_cols(preds_prob_sl) %>%
+  rename(sl_bycatch_class = .pred_class, sl_bycatch_prob_no = .pred_0, sl_bycatch_prob_yes = .pred_1)
+
+
+# seal
+preds_fac_hs <- predict(hs, predict_data_format)
+preds_prob_hs <- predict(hs, predict_data_format, type = "prob")
+preds_hs_final <- predict_data_format %>%
+  bind_cols(preds_fac_hs) %>%
+  bind_cols(preds_prob_hs) %>%
+  rename(hs_bycatch_class = .pred_class, hs_bycatch_prob_no = .pred_0, hs_bycatch_prob_yes = .pred_1)
+
+# soupfin shark
+preds_fac_ss <- predict(ss, predict_data_format)
+preds_prob_ss <- predict(ss, predict_data_format, type = "prob")
+preds_ss_final <- predict_data_format %>%
+  bind_cols(preds_fac_ss) %>%
+  bind_cols(preds_prob_ss) %>%
+  rename(ss_bycatch_class = .pred_class, ss_bycatch_prob_no = .pred_0, ss_bycatch_prob_yes = .pred_1)
+
+# save the result
+
+# sealion
+saveRDS(preds_sl_final, file.path("model_result/rf_prediction/rf_sl_spatial_pred.Rds"))
+
+# seal
+saveRDS(preds_hs_final, file.path("model_result/rf_prediction/rf_hs_spatial_pred.Rds"))
+
+# soupfin shark
+saveRDS(preds_ss_final, file.path("model_result/rf_prediction/rf_ss_spatial_pred.Rds") )
+
+###########################################################
+################### Figure making ######################
+###########################################################
+
+# Step 1: we want to have the set numbers after 2010
+
+logbook <- readRDS("data/confidential/processed/processed_CDFW_logbook/CDFW_1980_2022_gillnet_logbook_new_final.Rds")
+
+# wait should we weighted by sets? as we agree we can't identify sets from the logbook (but I'll keep going for now)
+
+# Step 2: calculate the weighted mean
+
+# Step 3: make figure
+
 
 
 
