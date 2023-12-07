f48debda8f588f741b30797040fc6c48ef9f8494
tinafang-1207 <yutianfang@ucsb.edu>
2023-12-06 18:13:50 -0800

    Finish the prediction function

--- code/modeling/Step_4_modeling_rf_prediction.R
+++ code/modeling/Step_4_modeling_rf_prediction.R
@@ -19,6 +19,11 @@ library(randomForest)
 
 data_predict <- readRDS("data/confidential/processed/pre_model/sealion/fake_spatial_pre_model.Rds")
 
+# read in the logbook data
+
+logbook <- readRDS("/Users/yutianfang/Dropbox/ca_set_gillnet_bycatch/confidential/logbooks/processed/CDFW_1981_2020_gillnet_logbook_data_use.Rds") 
+
+
 # read in model results
 
 # sealion
@@ -36,18 +41,17 @@ output_ss_weighted <- readRDS("model_result/weighted_rf/soupfin_shark_model_weig
 ### Extract the model best fit (to training data)
 
 # sealion - weight - 75
-sl <- output_sl_weighted[["final_fit"]][[3]]
+sl_best_fit <- output_sl_weighted[["final_fit"]][[3]]
 
 # harbor seal - SMOTE
-hs <- output_hs_balanced[["final_fit"]][["model_smote_final_fit"]]
+hs_best_fit <- output_hs_balanced[["final_fit"]][["model_smote_final_fit"]]
 
 # soupfin shark - SMOTE
-ss <- output_ss_balanced[["final_fit"]][["model_smote_final_fit"]]
+ss_best_fit <- output_ss_balanced[["final_fit"]][["model_smote_final_fit"]]
 
 ###########################################################
 ################### model prediction ######################
 ###########################################################
-
 
 
 # Format the predict data
@@ -65,94 +69,76 @@ predict_data_format <- data_predict %>%
   drop_na()
 
 
-#### list of the best model fit ####
-
-best_model_fit <- list(sl, hs, ss)
-
-
-best_model_fit = sl
-
-best_model_fit_run = sl
-
-predict_data = predict_data_format
-  
-# function to predict model
-predict_best_model <- function(best_model_fit, predict_data){
+# function to make spatial risk prediction
+predict_spatial_risk <- function(best_model_fit, predict_data, spp){
   
   preds_fac <- predict(best_model_fit, predict_data)
   preds_prob <- predict(best_model_fit, predict_data, type = "prob")
   
-  # weight will be vessel-day (will be the trip-id, use n_distinct(trip_id))
   
-  # combine columns
+  # combine columns to have final prediction dataframe
   predict_final <- predict_data %>%
     bind_cols(preds_fac) %>%
     bind_cols(preds_prob) %>%
-    rename(!!(paste0(gsub(best_model_fit), "_bycatch_class" )) := .pred_class, 
-           !!(paste0(gsub(best_model_fit), "_bycatch_prob_no")) := .pred_0,
-           !!(paste0(gsub(best_model_fit), "_bycatch_prob_yes")) := .pred_1)
+    mutate(species = spp) %>%
+    select(haul_lat_dd, haul_long_dd, julian_day, .pred_class, .pred_0, .pred_1, species)
+    
+  # calculate weight by julian day
   
-  return(predict_final)
+  # weight will be vessel-day (will be the trip-id, use n_distinct(trip_id))
+  logbook_weight <- logbook %>%
+    # create julian day
+    mutate(julian_day = lubridate::yday(date)) %>%
+    # filter
+    filter(year >=2010) %>%
+    filter(net_type == "Set") %>%
+    # count
+    group_by(julian_day) %>%
+    summarize(vessel_days_weight = n_distinct(trip_id))
+    
+  #combine the dataframe with weight and calculate the spatial risk
+  predict_spatial_risk <- predict_final %>%
+    left_join(logbook_weight, by = "julian_day") %>%
+    group_by(haul_lat_dd, haul_long_dd) %>%
+    summarize(spatial_risk = weighted.mean(.pred_1, vessel_days_weight)) %>%
+    mutate(species = spp)
+  
+  return(predict_spatial_risk)
 
 }
 
+# make prediction
 
-# this does not work now (need to change the column names!)
+sealion_spatial_risk <- predict_spatial_risk(best_model_fit = sl_best_fit, predict_data = predict_data_format, spp = "California sea lion")
+seal_spatial_risk <- predict_spatial_risk(best_model_fit = hs_best_fit, predict_data = predict_data_format, spp = "Harbor seal")
+soupfin_spatial_risk <- predict_spatial_risk(best_model_fit = ss_best_fit, predict_data = predict_data_format, spp = "Soupfin shark")
 
-# so I will just predict one by one (this will change later!)
+# combine the dataframe
+spatial_risk_final <- rbind(predict_spatial_risk,
+                            seal_spatial_risk,
+                            soupfin_spatial_risk)
 
-# sealion
+# save the combined dataframe
+saveRDS(spatial_risk_final, file.path("model_result/rf_prediction/spatial_risk_final.Rds"))
 
-preds_fac_sl <- predict(sl, predict_data_format)
-preds_prob_sl <- predict(sl, predict_data_format, type = "prob")
-preds_sl_final <-  predict_data_format %>%
-  bind_cols(preds_fac_sl) %>%
-  bind_cols(preds_prob_sl) %>%
-  rename(sl_bycatch_class = .pred_class, sl_bycatch_prob_no = .pred_0, sl_bycatch_prob_yes = .pred_1)
-
-
-# seal
-preds_fac_hs <- predict(hs, predict_data_format)
-preds_prob_hs <- predict(hs, predict_data_format, type = "prob")
-preds_hs_final <- predict_data_format %>%
-  bind_cols(preds_fac_hs) %>%
-  bind_cols(preds_prob_hs) %>%
-  rename(hs_bycatch_class = .pred_class, hs_bycatch_prob_no = .pred_0, hs_bycatch_prob_yes = .pred_1)
-
-# soupfin shark
-preds_fac_ss <- predict(ss, predict_data_format)
-preds_prob_ss <- predict(ss, predict_data_format, type = "prob")
-preds_ss_final <- predict_data_format %>%
-  bind_cols(preds_fac_ss) %>%
-  bind_cols(preds_prob_ss) %>%
-  rename(ss_bycatch_class = .pred_class, ss_bycatch_prob_no = .pred_0, ss_bycatch_prob_yes = .pred_1)
-
-# save the result
-
-# sealion
-saveRDS(preds_sl_final, file.path("model_result/rf_prediction/rf_sl_spatial_pred.Rds"))
-
-# seal
-saveRDS(preds_hs_final, file.path("model_result/rf_prediction/rf_hs_spatial_pred.Rds"))
-
-# soupfin shark
-saveRDS(preds_ss_final, file.path("model_result/rf_prediction/rf_ss_spatial_pred.Rds") )
-
-readRDS()
 
 ###########################################################
 ################### Figure making ######################
 ###########################################################
 
-# Step 1: we want to have the set numbers after 2010
 
-logbook <- readRDS("data/confidential/processed/processed_CDFW_logbook/CDFW_1980_2022_gillnet_logbook_new_final.Rds")
+# read in geographical reference data
 
-# wait should we weighted by sets? as we agree we can't identify sets from the logbook (but I'll keep going for now)
+usa <- rnaturalearth::ne_states(country = "United States of America", returnclass = "sf")
+mexico <- rnaturalearth::ne_countries(country="Mexico", returnclass = "sf")
 
-# Step 2: calculate the weighted mean
-
-# Step 3: make figure
+ggplot() +
+  geom_tile(data = soupfin_spatial_risk, aes(x = haul_long_dd, y = haul_lat_dd, fill = spatial_risk)) +
+  geom_sf(data = usa, fill = "grey85", col = "white", size = 0.2) +
+  geom_sf(data=mexico, fill="grey85", col="white", size=0.2) +
+  scale_fill_gradientn(name = "Spatial risk", colors = RColorBrewer::brewer.pal(9, "YlOrRd")) +
+  coord_sf(xlim = c(-121, -117), ylim = c(32, 35)) +
+  theme_bw()
 
 
 
