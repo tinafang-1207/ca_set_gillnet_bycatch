ffc9d02ea141b0346bcb1b3f86005b44d9e25c0a
tinafang-1207 <yutianfang@ucsb.edu>
2023-12-07 20:08:52 -0800

    update the prediction figures

--- code/modeling/Step_4_modeling_rf_prediction.R
+++ code/modeling/Step_4_modeling_rf_prediction.R
@@ -16,11 +16,9 @@ library(randomForest)
 # notice: this is the fake data to show the spatial pattern
 
 # The SST needs to fix in the future (we want to have average SST from 2010 rather than 2000)
-
 data_predict <- readRDS("data/confidential/processed/pre_model/sealion/fake_spatial_pre_model.Rds")
 
 # read in the logbook data
-
 logbook <- readRDS("/Users/yutianfang/Dropbox/ca_set_gillnet_bycatch/confidential/logbooks/processed/CDFW_1981_2020_gillnet_logbook_data_use.Rds") 
 
 
@@ -49,13 +47,15 @@ hs_best_fit <- output_hs_balanced[["final_fit"]][["model_smote_final_fit"]]
 # soupfin shark - SMOTE
 ss_best_fit <- output_ss_balanced[["final_fit"]][["model_smote_final_fit"]]
 
+# source the spatial risk predict function
+source("code/function/predict_rf.R")
+
 ###########################################################
 ################### model prediction ######################
 ###########################################################
 
 
 # Format the predict data
-
 predict_data_format <- data_predict %>%
   rename(haul_lat_dd = lats,
          haul_long_dd = longs,
@@ -69,44 +69,6 @@ predict_data_format <- data_predict %>%
   drop_na()
 
 
-# function to make spatial risk prediction
-predict_spatial_risk <- function(best_model_fit, predict_data, spp){
-  
-  preds_fac <- predict(best_model_fit, predict_data)
-  preds_prob <- predict(best_model_fit, predict_data, type = "prob")
-  
-  
-  # combine columns to have final prediction dataframe
-  predict_final <- predict_data %>%
-    bind_cols(preds_fac) %>%
-    bind_cols(preds_prob) %>%
-    mutate(species = spp) %>%
-    select(haul_lat_dd, haul_long_dd, julian_day, .pred_class, .pred_0, .pred_1, species)
-    
-  # calculate weight by julian day
-  
-  # weight will be vessel-day (will be the trip-id, use n_distinct(trip_id))
-  logbook_weight <- logbook %>%
-    # create julian day
-    mutate(julian_day = lubridate::yday(date)) %>%
-    # filter
-    filter(year >=2010) %>%
-    filter(net_type == "Set") %>%
-    # count
-    group_by(julian_day) %>%
-    summarize(vessel_days_weight = n_distinct(trip_id))
-    
-  #combine the dataframe with weight and calculate the spatial risk
-  predict_spatial_risk <- predict_final %>%
-    left_join(logbook_weight, by = "julian_day") %>%
-    group_by(haul_lat_dd, haul_long_dd) %>%
-    summarize(spatial_risk = weighted.mean(.pred_1, vessel_days_weight)) %>%
-    mutate(species = spp)
-  
-  return(predict_spatial_risk)
-
-}
-
 # make prediction
 
 sealion_spatial_risk <- predict_spatial_risk(best_model_fit = sl_best_fit, predict_data = predict_data_format, spp = "California sea lion")
@@ -114,9 +76,10 @@ seal_spatial_risk <- predict_spatial_risk(best_model_fit = hs_best_fit, predict_
 soupfin_spatial_risk <- predict_spatial_risk(best_model_fit = ss_best_fit, predict_data = predict_data_format, spp = "Soupfin shark")
 
 # combine the dataframe
-spatial_risk_final <- rbind(predict_spatial_risk,
+spatial_risk_final <- rbind(sealion_spatial_risk,
                             seal_spatial_risk,
-                            soupfin_spatial_risk)
+                            soupfin_spatial_risk) %>%
+  rename(Latitude = haul_lat_dd, Longitude = haul_long_dd)
 
 # save the combined dataframe
 saveRDS(spatial_risk_final, file.path("model_result/rf_prediction/spatial_risk_final.Rds"))
@@ -132,19 +95,81 @@ saveRDS(spatial_risk_final, file.path("model_result/rf_prediction/spatial_risk_f
 usa <- rnaturalearth::ne_states(country = "United States of America", returnclass = "sf")
 mexico <- rnaturalearth::ne_countries(country="Mexico", returnclass = "sf")
 
+# base theme
+base_theme <- theme(axis.text=element_text(size=7),
+                    axis.text.y = element_text(angle = 90, hjust = 0.5),
+                    axis.title=element_text(size=8),
+                    legend.text=element_text(size=6),
+                    legend.title=element_text(size=7),
+                    strip.text = element_text(size=8),
+                    plot.tag =element_text(size=9),
+                    plot.title=element_blank(),
+                    # Gridlines
+                    panel.grid.major = element_blank(),
+                    panel.grid.minor = element_blank(),
+                    panel.background = element_blank(),
+                    axis.line = element_line(colour = "black"),
+                    # Legend
+                    legend.key = element_rect(fill = NA),
+                    legend.background = element_rect(fill=alpha('blue', 0)))
+
 ggplot() +
-  geom_tile(data = soupfin_spatial_risk, aes(x = haul_long_dd, y = haul_lat_dd, fill = spatial_risk)) +
-  geom_sf(data = usa, fill = "grey85", col = "white", size = 0.2) +
-  geom_sf(data=mexico, fill="grey85", col="white", size=0.2) +
-  scale_fill_gradientn(name = "Spatial risk", colors = RColorBrewer::brewer.pal(9, "YlOrRd")) +
+  geom_tile(data = spatial_risk_final, aes(x = Longitude, y = Latitude, fill = spatial_risk)) +
+  geom_sf(data = usa, fill = "grey85", col = "white", linewidth=0.2, inherit.aes = F) +
+  geom_sf(data = mexico, fill = "grey85", col = "white", linewidth=0.2, inherit.aes = F) +
+  scale_fill_gradientn(name = "Spatial risk", colors = RColorBrewer::brewer.pal(9, "RdYlGn") %>% rev()) +
   coord_sf(xlim = c(-121, -117), ylim = c(32, 35)) +
-  theme_bw()
+  scale_x_continuous(breaks=seq(-122, -118, 1)) +
+  scale_y_continuous(breaks=seq(32, 35, 1)) +
+  facet_wrap(.~species) +
+  theme_bw() + base_theme
 
+g1 <- ggplot() +
+  geom_tile(data = spatial_risk_final %>% filter(species == "California sea lion"), aes(x = Longitude, y = Latitude, fill = spatial_risk)) +
+  geom_sf(data = usa, fill = "grey85", col = "white", linewidth=0.2, inherit.aes = F) +
+  geom_sf(data = mexico, fill = "grey85", col = "white", linewidth=0.2, inherit.aes = F) +
+  scale_fill_gradientn(name = "Spatial risk", colors = RColorBrewer::brewer.pal(9, "Spectral") %>% rev()) +
+  coord_sf(xlim = c(-121, -117), ylim = c(32, 35)) +
+  scale_x_continuous(breaks=seq(-122, -118, 1)) +
+  scale_y_continuous(breaks=seq(32, 35, 1)) +
+  facet_wrap(.~species) +
+  theme_bw() + base_theme
 
+g1
 
+g2 <- ggplot() +
+  geom_tile(data = spatial_risk_final %>% filter(species == "Harbor seal"), aes(x = Longitude, y = Latitude, fill = spatial_risk)) +
+  geom_sf(data = usa, fill = "grey85", col = "white", linewidth=0.2, inherit.aes = F) +
+  geom_sf(data = mexico, fill = "grey85", col = "white", linewidth=0.2, inherit.aes = F) +
+  scale_fill_gradientn(name = "Spatial risk", colors = RColorBrewer::brewer.pal(9, "Spectral") %>% rev()) +
+  coord_sf(xlim = c(-121, -117), ylim = c(32, 35)) +
+  scale_x_continuous(breaks=seq(-122, -118, 1)) +
+  scale_y_continuous(breaks=seq(32, 35, 1)) +
+  facet_wrap(.~species) +
+  theme_bw() + base_theme
 
+g2
 
+g3 <- ggplot() +
+  geom_tile(data = spatial_risk_final %>% filter(species == "Soupfin shark"), aes(x = Longitude, y = Latitude, fill = spatial_risk)) +
+  geom_sf(data = usa, fill = "grey85", col = "white", linewidth=0.2, inherit.aes = F) +
+  geom_sf(data = mexico, fill = "grey85", col = "white", linewidth=0.2, inherit.aes = F) +
+  scale_fill_gradientn(name = "Spatial risk", colors = RColorBrewer::brewer.pal(9, "Spectral") %>% rev()) +
+  coord_sf(xlim = c(-121, -117), ylim = c(32, 35)) +
+  scale_x_continuous(breaks=seq(-122, -118, 1)) +
+  scale_y_continuous(breaks=seq(32, 35, 1)) +
+  facet_wrap(.~species) +
+  theme_bw() + base_theme
 
+g3
 
+g <- gridExtra::grid.arrange(g1, g2, g3, ncol = 3)
+
+g
+
+plotdir <- "figures"
+
+ggsave(g, filename=file.path(plotdir, "Fig7_spatial_prediction.png"), 
+       width=7, height=6.5, units="in", dpi=600)
 
 
