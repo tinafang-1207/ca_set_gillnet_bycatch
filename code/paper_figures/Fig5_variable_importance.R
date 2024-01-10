e39f39db6fedaf313818592c9a53d034f4ef2ba1
tinafang-1207 <yutianfang@ucsb.edu>
2024-01-09 18:28:23 -0800

    code updates

--- code/paper_figures/Fig5_variable_importance.R
+++ code/paper_figures/Fig5_variable_importance.R
@@ -19,16 +19,16 @@ data <- read_csv("model_result/variable_importance.csv")
 
 data <- data %>%
   mutate(variable = recode_factor(variable, 
-                                "net_mesh_size_in" = "Mesh size", 
-                                "haul_long_dd" = "Longitude",
-                                "haul_lat_dd" = "Latitude",
-                                "sst" = "Temperature",
-                                "julian_day" = "Julian day",
-                                "haul_depth_fa" = "Depth",
-                                "dist_km" = "Shore distance",
-                                "soak_hr" = "Soak hour")) %>%
+                                  "yday"="Julian day",      
+                                  "sst_c"="Temperature",             
+                                  "lat_dd"="Latitude",
+                                  "mesh_size_in"="Mesh size",
+                                  "shore_km"="Shore distance",
+                                  "depth_fa"="Depth",    
+                                  "soak_hr"="Soak hour",
+                                  "island_yn" = "Island location (yes or no)")) %>%
   mutate(category = case_when(variable %in% c("Mesh size", "Soak hour")~"Fishing",
-                              variable %in% c("Latitude", "Longitude", "Depth", "Shore distance")~"Space",
+                              variable %in% c("Latitude", "Depth", "Shore distance", "Island location (yes or no)")~"Space",
                               variable %in% c("Julian day")~"Time",
                               variable%in% c("Temperature")~"Environment"))
 
@@ -53,7 +53,7 @@ base_theme <-  theme(axis.text=element_text(size=7),
 g <- ggplot(data, aes(x=importance, 
                       y=tidytext::reorder_within(variable, importance, species),
                       fill = category)) +
-  facet_wrap(~species, ncol=3, scales="free") +
+  facet_wrap(~species, ncol=4, scales="free") +
   geom_bar(stat="identity") +
   scale_fill_discrete(name = "Category") +
   # Labels
@@ -66,6 +66,6 @@ g
 
 # Export
 ggsave(g, filename=file.path(plotdir, "Fig5_variable_importance.png"), 
-       width=6.5, height=3, units="in", dpi=600)
+       width=12, height=3, units="in", dpi=600)
 
 
