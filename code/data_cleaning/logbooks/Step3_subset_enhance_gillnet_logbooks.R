3f30f8e4f8ece4ecc998ae92998b94b66ae5f98f
cfree14 <cfree14@gmail.com>
2024-02-01 16:09:10 -0800

    logbook work

--- code/data_cleaning/logbooks/Step3_subset_enhance_gillnet_logbooks.R
+++ code/data_cleaning/logbooks/Step3_subset_enhance_gillnet_logbooks.R
@@ -18,6 +18,15 @@ data_orig <- readRDS(file=file.path(outdir, "CDFW_1981_2020_gillnet_logbook_data
 
 # Read block key
 block_key <- readRDS("data/bathymetry/processed/block_key.Rds")
+strata_key <- readRDS("data/strata/block_strata_key.Rds") %>% 
+  # Island if in CI strata or if four blocks around Farralon Islands
+  mutate(island_yn=strata=="Channel Islands" | block_id %in% c(457, 458,467, 468)) %>% 
+  select(-strata)
+
+# Read block temperature key
+sst_key <- readRDS("data/gis_data/1981_2022_block_sst_expperience.Rds") %>% 
+  select(-sst_c) %>% 
+  rename(sst_c=sst_c_imp)
 
 
 # Build data
@@ -29,8 +38,22 @@ data <- data_orig %>%
   filter(net_type=="Set" & mesh_in >= 3.5) %>%
   # Add block distance from shore
   left_join(block_key %>% select(block_id, shore_km), by="block_id") %>% 
+  # Add julian day
+  mutate(yday=lubridate::yday(date)) %>% 
+  # Add island y/n
+  left_join(strata_key, by="block_id") %>% 
+  # mutate(island_yn=ifelse(is.na(island_yn), F, island_yn)) %>% 
+  # Add SST
+  left_join(sst_key, by=c("date", "block_id")) %>% 
   # Arrange
-  select(logbook_id:block_long_dd, shore_km, everything())
+  relocate(yday, .after=date) %>% 
+  relocate(shore_km, .after=block_long_dd) %>% 
+  relocate(island_yn, .after=shore_km) %>% 
+  relocate(sst_c, .after=island_yn)
+
+# Inspect
+freeR::complete(data)
+
 
 
 # Build data
