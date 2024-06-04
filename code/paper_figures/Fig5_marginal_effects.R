e3df86a9007e7fa6d55fbe56c1651e2330e0811f
tinafang-1207 <yutianfang@ucsb.edu>
2024-06-04 16:35:18 -0700

    Figure updates

--- code/paper_figures/Fig5_marginal_effects.R
+++ code/paper_figures/Fig5_marginal_effects.R
@@ -44,12 +44,17 @@ base_theme <-  theme(axis.text=element_text(size=7),
 
 # Plot
 
+# specify the species color
 species_color <- c("California sea lion" = "#B83945", "Common murre" = "#4F845C", "Harbor seal" = "#E3E457", "Soupfin shark" = "#377483")
 
+# create dataframe of vertical dashed lines
+vline <- data.frame(xintercept = c(40, 60, 3),
+                    variable = c("Depth (fathoms)", "Depth (fathoms)", "Shore distance (km)"))
 
 main_plot <- ggplot(data, aes(x=value, y=prob, color=species)) +
   facet_wrap(~variable, ncol=3, scales="free") +
   geom_line() +
+  geom_vline(data = vline, aes(xintercept = xintercept), linetype = "dashed", color = "gray") +
   # Labels
   labs(x="Variable value", y="Marginal effect") +
   # Legend
@@ -90,213 +95,6 @@ plot_with_insert
 ggsave(plot_with_insert, filename=file.path(plotdir, "Fig5_marginal_effects.png"), 
        width=5.6, height=6.5, units="in", dpi=600)
 
-
-
-######################## by species ############################
-
-# plot directory
-plotdir <- "presentations"
-
-
-# Theme
-base_theme <-  theme(axis.text=element_text(size=7),
-                     axis.title=element_text(size=8),
-                     legend.text=element_text(size=7),
-                     legend.title=element_text(size=8),
-                     strip.text=element_text(size=8),
-                     # Gridlines
-                     panel.grid.major = element_blank(), 
-                     panel.grid.minor = element_blank(),
-                     panel.background = element_blank(), 
-                     axis.line = element_line(colour = "black"),
-                     # Legend
-                     legend.background = element_rect(fill=alpha('blue', 0)))
-
-
-# California sea lion
-
-species_color <- c("California sea lion" = "#B83945", "Common murre" = "#4F845C", "Harbor seal" = "#E3E457", "Soupfin shark" = "#377483")
-
-main_plot <- ggplot(data %>% filter(species == "California sea lion"), aes(x=value, y=prob, color=species)) +
-  facet_wrap(~variable, ncol=4, scales="free") +
-  geom_line() +
-  # Labels
-  labs(x="Variable value", y="Marginal effect") +
-  # Legend
-  scale_colour_manual(name="Species", values = species_color) +
-  # Theme
-  theme_bw() + base_theme +
-  theme(legend.key.size=unit(0.3, "cm"),
-        legend.position = "bottom")
-
-main_plot
-
-
-insert_plot <- ggplot(data_cat %>% filter(species == "California sea lion"), aes(x = value, y = prob, fill=species)) +
-  facet_wrap(~variable) +
-  geom_bar(stat = "identity", position = "dodge") +
-  # fill color
-  scale_fill_manual(values = species_color) +
-  # Theme
-  theme_bw() + base_theme +
-  theme(legend.position = "none",
-        axis.title.x = element_blank(),
-        axis.title.y = element_blank())
-
-insert_plot
-
-# use cowplot package, we can insert categorical plot inside
-
-plot_with_insert <-
-  ggdraw() +
-  draw_plot(main_plot) +
-  draw_plot(insert_plot, x = 0.755, y = 0.125, width = 0.25, height = 0.45)
-
-plot_with_insert
-
-# Export
-ggsave(plot_with_insert, filename=file.path(plotdir, "Fig5_marginal_effects_SL.png"), 
-       width=5.6, height=4.19, units="in", dpi=600)
-
-########################################################################
-
-# Harbor seal
-
-
-main_plot <- ggplot(data %>% filter(species == "Harbor seal"), aes(x=value, y=prob, color=species)) +
-  facet_wrap(~variable, ncol=4, scales="free") +
-  geom_line() +
-  # Labels
-  labs(x="Variable value", y="Marginal effect") +
-  # Legend
-  scale_colour_manual(name="Species", values = species_color) +
-  # Theme
-  theme_bw() + base_theme +
-  theme(legend.key.size=unit(0.3, "cm"),
-        legend.position = "bottom")
-
-main_plot
-
-
-insert_plot <- ggplot(data_cat %>% filter(species == "Harbor seal"), aes(x = value, y = prob, fill=species)) +
-  facet_wrap(~variable) +
-  geom_bar(stat = "identity", position = "dodge") +
-  # fill color
-  scale_fill_manual(values = species_color) +
-  # Theme
-  theme_bw() + base_theme +
-  theme(legend.position = "none",
-        axis.title.x = element_blank(),
-        axis.title.y = element_blank())
-
-insert_plot
-
-# use cowplot package, we can insert categorical plot inside
-
-plot_with_insert <-
-  ggdraw() +
-  draw_plot(main_plot) +
-  draw_plot(insert_plot, x = 0.755, y = 0.125, width = 0.25, height = 0.45)
-
-plot_with_insert
-
-# Export
-ggsave(plot_with_insert, filename=file.path(plotdir, "Fig5_marginal_effects_HS.png"), 
-       width=5.6, height=4.19, units="in", dpi=600)
-
-########################################################################
-
-
-# Common murre
-
-
-main_plot <- ggplot(data %>% filter(species == "Common murre"), aes(x=value, y=prob, color=species)) +
-  facet_wrap(~variable, ncol=4, scales="free") +
-  geom_line() +
-  # Labels
-  labs(x="Variable value", y="Marginal effect") +
-  # Legend
-  scale_colour_manual(name="Species", values = species_color) +
-  # Theme
-  theme_bw() + base_theme +
-  theme(legend.key.size=unit(0.3, "cm"),
-        legend.position = "bottom")
-
-main_plot
-
-
-insert_plot <- ggplot(data_cat %>% filter(species == "Common murre"), aes(x = value, y = prob, fill=species)) +
-  facet_wrap(~variable) +
-  geom_bar(stat = "identity", position = "dodge") +
-  # fill color
-  scale_fill_manual(values = species_color) +
-  # Theme
-  theme_bw() + base_theme +
-  theme(legend.position = "none",
-        axis.title.x = element_blank(),
-        axis.title.y = element_blank())
-
-insert_plot
-
-# use cowplot package, we can insert categorical plot inside
-
-plot_with_insert <-
-  ggdraw() +
-  draw_plot(main_plot) +
-  draw_plot(insert_plot, x = 0.755, y = 0.125, width = 0.25, height = 0.45)
-
-plot_with_insert
-
-# Export
-ggsave(plot_with_insert, filename=file.path(plotdir, "Fig5_marginal_effects_CM.png"), 
-       width=5.6, height=4.19, units="in", dpi=600)
-
-########################################################################
-
-
-# Soupfin shark
-
-
-main_plot <- ggplot(data %>% filter(species == "Soupfin shark"), aes(x=value, y=prob, color=species)) +
-  facet_wrap(~variable, ncol=4, scales="free") +
-  geom_line() +
-  # Labels
-  labs(x="Variable value", y="Marginal effect") +
-  # Legend
-  scale_colour_manual(name="Species", values = species_color) +
-  # Theme
-  theme_bw() + base_theme +
-  theme(legend.key.size=unit(0.3, "cm"),
-        legend.position = "bottom")
-
-main_plot
-
-
-insert_plot <- ggplot(data_cat %>% filter(species == "Soupfin shark"), aes(x = value, y = prob, fill=species)) +
-  facet_wrap(~variable) +
-  geom_bar(stat = "identity", position = "dodge") +
-  # fill color
-  scale_fill_manual(values = species_color) +
-  # Theme
-  theme_bw() + base_theme +
-  theme(legend.position = "none",
-        axis.title.x = element_blank(),
-        axis.title.y = element_blank())
-
-insert_plot
-
-# use cowplot package, we can insert categorical plot inside
-
-plot_with_insert <-
-  ggdraw() +
-  draw_plot(main_plot) +
-  draw_plot(insert_plot, x = 0.755, y = 0.125, width = 0.25, height = 0.45)
-
-plot_with_insert
-
-# Export
-ggsave(plot_with_insert, filename=file.path(plotdir, "Fig5_marginal_effects_SS.png"), 
-       width=5.6, height=4.19, units="in", dpi=600)
 
 
 
