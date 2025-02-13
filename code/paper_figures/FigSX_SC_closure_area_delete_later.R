88d4a9e933ce2092ea3a356e92d9ceed6c0e9511
cfree14 <cfree14@gmail.com>
2025-02-12 16:01:04 -0800

    adding gillnet exclusion areas

--- code/paper_figures/FigSX_SC_closure_area_delete_later.R
+++ code/paper_figures/FigSX_SC_closure_area_delete_later.R
@@ -8,13 +8,59 @@ library(tidyverse)
 # read in data
 state_water <- readRDS("data/gis_data/CA_state_waters_polyline.Rds")
 islands <- sf::st_read("data/gis_data/california_islands/california_islands.shp")
-# bathy <- raster::raster("data/gis_data/bd200m_v2i")
-bathy_spatial <- read.csv("data/gis_data/bathy_extracted.csv")
+bathy <- raster::raster("data/gis_data/bd200m_v2i")
+# bathy_spatial <- read.csv("data/gis_data/bathy_extracted.csv")
 
 usa <- rnaturalearth::ne_states(country = "United States of America", returnclass = "sf")
 mexico <- rnaturalearth::ne_countries(country="Mexico", returnclass = "sf")
 
 wgs84 <- "+proj=longlat +datum=WGS84"
+
+
+####
+
+## Directories
+base.dir <- "/Users/cfree/Library/CloudStorage/GoogleDrive-cfree@ucsb.edu/.shortcut-targets-by-id/1kCsF8rkm1yhpjh2_VMzf8ukSPf9d4tqO/MPA Network Assessment: Working Group Shared Folder/data/sync-data"
+data.dir <- file.path(base.dir, "gis_data/raw")
+out.dir <- file.path(base.dir, "gis_data/processed")
+
+
+## Get depth data
+## Source: https://filelib.wildlife.ca.gov/Public/R7_MR/BATHYMETRY/
+depth_raw <- stars::read_stars(file.path(data.dir, "200mEEZ_BathyGrids", "bd200m_v2i", "w001001.adf")) # meters
+depth_raw_fa <- stars::read_stars(file.path(data.dir, "200mEEZ_BathyGrids", "bd200fa_v2i", "w001001.adf")) # fathoms
+
+# Convert from STARS to terra
+depth_fa1 <- terra::rast(depth_raw_fa)
+
+# Reproject
+# depth_fa <- sf::st_transform(depth_raw_fa, crs = sf::st_crs("+proj=longlat +datum=WGS84"))
+
+# Plot check
+# raster::plot(depth_raw_fa)
+
+# Generate contour lines at the target depth
+contour <- terra::as.contour(depth_fa1, levels = -70)
+
+# Convert contour to sf object
+contour_sf <- sf::st_as_sf(contour) %>%
+  # Break
+  sf::st_cast("LINESTRING") %>% 
+  # Add id
+  mutate(id=1:nrow(.)) %>% 
+  # Remove mainland
+  filter(id!=1) %>% 
+  # Reproject
+  sf::st_transform(crs = sf::st_crs("+proj=longlat +datum=WGS84")) %>% 
+  # Break
+  sf::st_cast('POLYGON')
+
+# Export
+sf::st_write(contour_sf, dsn="data/gis_data/70fathom_contour.shp")
+
+ggplot() +
+  geom_sf(data=contour_sf, mapping=aes(fill=as.character(id)))
+
 
 ##############################################################################
 
@@ -27,7 +73,18 @@ buffer <- islands_poly %>%
   sf::st_union() %>%
   sf::st_transform(wgs84)
 
+
+ggplot() +
+  geom_sf(data=islands_poly %>% sf::st_transform(wgs84), fill="black") +
+  geom_sf(data=contour_sf, fill=NA) +
+  geom_sf(data=buffer, color="red", fill=NA) +
+  coord_sf(ylim=c(32.7, 34.2), xlim=c(-120.5, -118))
+
+
+
 #############################################################################
+
+raster::plot(bathy)
 
 # extract the bathymetry data
 
